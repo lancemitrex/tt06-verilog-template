54a8ea9bcadbe32a016b00ffef3fc9a945515ead
lancemitrex <lancemitrex@users.noreply.github.com>
2024-04-19 13:00:47 -0400

    Update tb.v  trimmed name to tt_um_lancemitrex

--- test/tb.v
+++ test/tb.v
@@ -23,7 +23,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_lancemitrex_DIP_Switch_to_HEX_Display
+  tt_um_lancemitrex
  user_project (
 
       // Include power ports for the Gate Level test:
